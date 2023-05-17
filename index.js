3a4cf6602443ac8f7ce4193f048bf6781e04de78
aleksandr912 <45685696+aleksandr912@users.noreply.github.com>
2023-05-17 15:02:19 +0300

    add options.js

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
 const TelegramApi = require("node-telegram-bot-api");
+const { gameOptions, againOptions } = require("./options");
 
 const token = "6279702532:AAEp2G8zxUI85hIi61bGOYsatztsDVdlDNw";
 
 const bot = new TelegramApi(token, { polling: true });
 
 const chats = {};
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: "1", callback_data: "1" },
-        { text: "2", callback_data: "2" },
-        { text: "3", callback_data: "3" },
-      ],
-      [
-        { text: "4", callback_data: "4" },
-        { text: "5", callback_data: "5" },
-        { text: "6", callback_data: "6" },
-      ],
-      [
-        { text: "7", callback_data: "7" },
-        { text: "8", callback_data: "8" },
-        { text: "9", callback_data: "9" },
-      ],
-    ],
-  }),
-};
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "Играть еще раз", callback_data: "/again" }]],
-  }),
-};
 
 const startGame = async (chatId) => {
   await bot.sendMessage(
